aca4279306603794d413c75c5f5886c9f11580a9
Natraj Padwani <nextronickk@gmail.com>
2024-04-28 05:33:40 +0800

    cors

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const { randomWordwithDictionary } = require("./controller/word");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://nextronick.github.io/word-of-the-day/",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.get("/word-random", randomWordwithDictionary);
 
